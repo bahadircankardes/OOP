3f3f1ed0770ec8fcb6c63a62003749e9d2767af3
bahadircankardes <kardes126@gmail.com>
2024-09-19 13:13:05 +0300

    gc

--- 2-ClassOrnekleri/Program.cs
+++ 2-ClassOrnekleri/Program.cs
@@ -26,6 +26,27 @@ class Program
             IslemciTipi = IslemciTipi.Intel
         };
 
+        AnaKart.Marka = "Asus";
+
+        /*
+         
+         */
+
+        Kasa kasa = new Kasa();
+        kasa.Marka = "Asus";
+        kasa.Model = "xvc";
+
+        Kasa kasa2 = new Kasa();
+        kasa2 = kasa;
+        kasa2.Marka = "ABC";
+
+        Console.WriteLine(kasa.Marka);
+        Console.WriteLine(kasa2.Marka);
+
+        kasa2.Model = "QWE";
+        Console.WriteLine(kasa.Model);
+        Console.WriteLine(kasa2.Model);
+
         #endregion
 
         WissenComputer computer = new WissenComputer();
