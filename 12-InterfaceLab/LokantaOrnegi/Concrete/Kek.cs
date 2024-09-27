732fef6297e37309edbc5c0e640b36e345bfa3a9
bahadircankardes <kardes126@gmail.com>
2024-09-27 21:59:35 +0300

    Lokanta

---
+++ 12-InterfaceLab/LokantaOrnegi/Concrete/Kek.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using _12_InterfaceLab.LokantaOrnegi.Abstract;
+
+namespace _12_InterfaceLab.LokantaOrnegi.Concrete
+{
+	public class Kek:IPisirilebilir
+	{
+        public void pisir()
+        {
+            Console.WriteLine($"{this.GetType().Name} Pisti...");
+        }
+    }
+}
+
--- 12-InterfaceLab/Program.cs
+++ 12-InterfaceLab/Program.cs
@@ -15,18 +15,56 @@ class Program
         Salata salata = new Salata();
         TazeFasulye tazeFasulye = new TazeFasulye();
         Ispanak ispanak = new Ispanak();
+        Kek kek = new Kek();
 
-        List<IPisirilebilir> pisirileiblirs = new List<IPisirilebilir>();
-        pisirileiblirs.Add(pilav);
-        pisirileiblirs.Add(ispanak);
-        pisirileiblirs.Add(tazeFasulye);
-
+        
+        
+        List<IPisirilebilir> pisirilebilirs = new List<IPisirilebilir>();
         List<IYapilabilir> yapilabilirs = new List<IYapilabilir>();
-        yapilabilirs.Add(cacik);
-        yapilabilirs.Add(salata);
-
         BorsaLokantasi borsaLokantasi = new BorsaLokantasi();
-        borsaLokantasi.YemekPisir(pisirileiblirs);
+        string secim = "";
+        do
+        {
+            Console.WriteLine("Menü Secimi Yapiniz \n 1-Kofte \n 2-Pilav \n 3-Salata \n 0-Cıkıs");
+            secim = Console.ReadLine();
+
+            if (secim == "1")
+            {
+                pisirilebilirs.Add(kofte);
+                
+            }
+            else if (secim =="2")
+            {
+                pisirilebilirs.Add(pilav);
+                
+            }
+            else if (secim == "3")
+            {
+                yapilabilirs.Add(salata);
+                
+            }
+            
+
+        } while (secim!="0");
+
+        borsaLokantasi.YemekPisir(pisirilebilirs);
+        borsaLokantasi.YemekYap(yapilabilirs);
+
+
+
+
+        //List<IPisirilebilir> pisirileiblirs = new List<IPisirilebilir>();
+        //pisirileiblirs.Add(pilav);
+        //pisirileiblirs.Add(ispanak);
+        //pisirileiblirs.Add(tazeFasulye);
+        //pisirileiblirs.Add(kek);
+
+        //List<IYapilabilir> yapilabilirs = new List<IYapilabilir>();
+        //yapilabilirs.Add(cacik);
+        //yapilabilirs.Add(salata);
+
+        //BorsaLokantasi borsaLokantasi = new BorsaLokantasi();
+        //borsaLokantasi.YemekPisir(pisirileiblirs);
         //borsaLokantasi.YemekYap(yapilabilirs);
     }
 }
